24a0a8889edd433da1db6b0bd186e98004b2eb1e
fisheeesh <swanphyo444@gmail.com>
2024-12-08 02:09:57 +0700

    provoke validation trigger

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -74,8 +74,8 @@ export default {
           max_value: `This field ${ctx.field} must be at most 100.`,
           excluded: `You are not allowed ot use this value for this field ${ctx.field}.`,
           country_excluded: `Due to restrictions, we do not accept users from this location.`,
-          passwords_mismatch : `The passwords don't match.`,
-          tos : `You must accept the Terms of Service.`
+          passwords_mismatch: `The passwords don't match.`,
+          tos: `You must accept the Terms of Service.`,
         }
 
         /**
@@ -83,10 +83,39 @@ export default {
          * ? So, we want to check if a message for this rule exists in our messages object
          * ? If does, we will set the message vari to it. If not, we will have a generic message
          */
-        const message = messages[ctx.rule.name] ? messages[ctx.rule.name] : `The field ${ctx.field} is invalid.`
+        const message = messages[ctx.rule.name]
+          ? messages[ctx.rule.name]
+          : `The field ${ctx.field} is invalid.`
 
         return message
       },
+
+      /**
+       * !! A totasl of 4 options to change the validaation triggers
+       * !! We are using the default values cus the default behavior works fine for our case
+       */
+      /**
+       * ? The validateOnBlur option will tell vee-validate if it should validate a field on the blur event.
+       * ? By default, this opiton is set to true
+       */
+      validateOnBlur: true,
+      /**
+       * ? The validateOnChange option will tell vee-validate if it should validate a field on the change event.
+       * ? By default, this opiton is set to true
+       */
+      validateOnChange: true,
+      /**
+       * ? This option is the most aggressive validation trigger, the input event is fired whenever the input changes
+       * $ Its's different from the change event cuz it'll fire on every keystroke
+       * $ The momemnt the user presses a key if the value is invalid and error will appear immediately
+       * $ This option may be appropriate for inputs like passwords but it's excessive for most inputs
+       * ! User dun like to have errors thrown at them immediately
+       */
+      validateOnInput: false,
+      /**
+       * ? This option will tell vee-validate to validate the input whenever the value changes internally through v-modal directive
+       */
+      validateOnModelUpdate: true,
     })
   },
 }
