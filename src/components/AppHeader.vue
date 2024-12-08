2010fb35357b0d01c169263567bd71b5e8a947ff
fisheeesh <swanphyo444@gmail.com>
2024-12-09 00:39:02 +0700

    adding routes

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,25 +2,30 @@
   <header id="header" class="bg-gray-700">
     <nav class="container flex items-center justify-start px-4 py-5 mx-auto">
       <!-- App Name -->
-      <a class="mr-4 text-2xl font-bold text-white uppercase" href="#">Music</a>
+      <router-link exact-active-class="no-active" :to="{ name: 'home' }" class="mr-4 text-2xl font-bold text-white uppercase">Music</router-link>
 
       <div class="flex items-center flex-grow">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
+          <li>
+            <router-link class="px-2 text-white" :to="{ name: 'about' }">About</router-link>
+          </li>
           <!-- Navigation Links -->
           <li v-if="!userStore.userLoggedIn">
             <button class="px-2 text-white focus:outline-none form-option" @click.prevent="toggleAuthModal">
               Login / Register
             </button>
           </li>
+          <!-- By wrapping it with the template tags, we'll be able ot render multiple list items -->
           <template v-else>
             <li>
-              <button type="button" class="px-2 text-white hover:text-gray-300 focus:outline-none form-option" @click="handleManage">
+              <router-link to="/manage" class="px-2 text-white hover:text-gray-300 focus:outline-none form-option">
                 Manage
-              </button>
+              </router-link>
             </li>
             <li>
-              <button @click="userStore.signOut" type="button" class="px-2 text-white hover:text-gray-300 focus:outline-none form-option">
+              <button @click="userStore.signOut" type="button"
+                class="px-2 text-white hover:text-gray-300 focus:outline-none form-option">
                 LogOut
               </button>
             </li>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+import ManageMusic from '@/views/ManageMusic.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView,
+  },
+  {
+    path: '/manage',
+    name: 'manage',
+    component: ManageMusic,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    }
-  ],
+  routes,
+  linkExactActiveClass: 'text-yellow-500',
 })
 
 export default router
