e24004f1bff086c3e5222090357fde25b4aca802
fisheeesh <swanphyo444@gmail.com>
2024-12-22 18:57:04 +0700

    add spinner during scroll fetching time

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -28,9 +28,14 @@
         <ol id="playlist">
           <SongItem v-for="song in songs" :key="song.docID" :song="song" />
         </ol>
-        <!-- .. end Playlist -->
       </div>
     </section>
+    <div v-if="pendingRequest" class="py-4 my-3 text-center">
+      <PulseLoader />
+    </div>
+    <div v-if="noMoreSongs" class="py-4 text-center text-gray-500">
+      <span>{{ $t('home.no_songs') }}</span>
+    </div>
   </main>
 </template>
 
@@ -38,72 +43,93 @@
 import SongItem from '@/components/song/SongItem.vue';
 import { songsCollection } from '@/includes/firebase';
 import { onBeforeUnmount, onMounted, ref } from 'vue';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 
-const songs = ref([])
-const maxPerPage = ref(25)
-const pendingRequest = ref(false)
-
+const songs = ref([]);
+const maxPerPage = ref(7);
+const pendingRequest = ref(false);
+const noMoreSongs = ref(false);
 
 onMounted(() => {
-  getSongs()
+  getSongs();
 
-  window.addEventListener('scroll', handleScroll)
-})
+  window.addEventListener('scroll', handleScroll);
+});
 
 onBeforeUnmount(() => {
-  window.removeEventListener('scroll', handleScroll)
-})
+  window.removeEventListener('scroll', handleScroll);
+});
 
 const handleScroll = () => {
-  const { scrollTop, offsetHeight } = document.documentElement
-  const { innerHeight } = window
   /**
-   * $ Alternative Option Solution (less strict)
-   * $ const bottomOfWindow = Math.round(scrollTop) + innerHeight > offsetHeight -100
-   * ? scrollTop has to wrap with Math.round() cuz it may have decimal number
-   */
-  const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight
+   * ? Stop fetching if there are no more songs or if a request is already in progress
+   *  */
+  if (noMoreSongs.value || pendingRequest.value) {
+    return;
+  }
+
+  const { scrollTop, offsetHeight } = document.documentElement;
+  const { innerHeight } = window;
+  const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight;
+
+  /**
+ * $ Alternative Option Solution (less strict)
+ * $ const bottomOfWindow = Math.round(scrollTop) + innerHeight > offsetHeight -100
+ * ? scrollTop has to wrap with Math.round() cuz it may have decimal number
+ */
 
   if (bottomOfWindow) {
-    console.log('Bottom of window')
-    getSongs()
-
+    console.log('Bottom of window');
+    getSongs();
   }
-}
+};
+
 const getSongs = async () => {
-  if (pendingRequest.value) {
-    return
+  /**
+   * ? Prevent fetching if already pending or no more songs to load
+   */
+  if (pendingRequest.value || noMoreSongs.value) {
+    return;
   }
 
-  pendingRequest.value = true
+  pendingRequest.value = true;
 
   let snapshots;
 
   if (songs.value.length) {
-    const lastDoc = await songsCollection.doc(songs.value[songs.value.length - 1].docID).get()
+    const lastDoc = await songsCollection.doc(songs.value[songs.value.length - 1].docID).get();
     snapshots = await songsCollection
       .orderBy('modified_name')
       .startAfter(lastDoc)
       .limit(maxPerPage.value)
-      .get()
-  }
-  else {
+      .get();
+  } else {
     snapshots = await songsCollection
       .orderBy('modified_name')
       .limit(maxPerPage.value)
-      .get()
+      .get();
   }
 
-  snapshots.forEach(doc => {
-    songs.value.push({
-      docID: doc.id,
-      ...doc.data()
-    })
-  })
+  if (snapshots.empty) {
+    noMoreSongs.value = true;
+  } else {
+    snapshots.forEach(doc => {
+      songs.value.push({
+        docID: doc.id,
+        ...doc.data()
+      });
+    });
+  }
 
-  pendingRequest.value = false
-}
+  /**
+   * ?If fewer songs are fetched than the maxPerPage limit, mark that there are no more songs
+   */
+  if (snapshots.size < maxPerPage.value) {
+    noMoreSongs.value = true;
+  }
 
+  pendingRequest.value = false;
+};
 </script>
 
 <style></style>
